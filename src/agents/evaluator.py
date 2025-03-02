d27e2d2eb78867448359a35bb269092374f83813
Ema Suriano <emanuel.suriano@gmail.com>
2025-03-02 22:52:16 +0100

    improve evaluator

--- src/agents/evaluator.py
+++ src/agents/evaluator.py
@@ -20,7 +20,8 @@ OLLAMA_URL = os.getenv("OLLAMA_URL")
 assert OLLAMA_MODEL is not None, "OLLAMA_MODEL is not set"
 assert OLLAMA_URL is not None, "OLLAMA_URL is not set"
 
-llm = OllamaLLM(model=OLLAMA_MODEL, base_url=OLLAMA_URL, temperature=0)
+llm_feedback = OllamaLLM(model=OLLAMA_MODEL, base_url=OLLAMA_URL, temperature=0.5)
+llm = OllamaLLM(model=OLLAMA_MODEL, base_url=OLLAMA_URL, temperature=0, num_predict=1)
 
 
 class ConversationContext(BaseModel):
@@ -41,6 +42,16 @@ class ConversationMetrics(BaseModel):
     fluency: int = 50
 
 
+metrics = [
+    {"name": "grammar", "description": "Grammatical accuracy"},
+    {
+        "name": "vocabulary",
+        "description": "Appropriate use of vocabulary for level",
+    },
+    {"name": "fluency", "description": "Natural and smooth communication"},
+]
+
+
 def _generate_system_prompt_new(
     context: ConversationContext,
 ) -> str:
@@ -50,8 +61,30 @@ def _generate_system_prompt_new(
         f"""Role: You are a language evaluator, and you are evaluating a conversation between a user and a system.
 
         User Level: {cefr_level}
+        User language: {context.user.current_language.name}
      """
     )
+
+
+async def _get_example_phrases(
+    situation: schemas.SituationSystem,
+    rag_evaluator: RAGLanguageEvaluator,
+    user: schemas.User,
+) -> List[LanguageExample]:
+    context = [situation.name, situation.scenario_description, *situation.user_goals]
+    examples = rag_evaluator.get_relevant_examples(
+        query=" ".join(context),
+        level=CEFR_LEVEL[user.language_level - 1],
+        k=5,
+    )
+
+    for example in examples:
+        translated_phrase = await translate_text(
+            example.phrase, user.current_language, user.language_level
+        )
+        example.phrase = translated_phrase
+
+    return examples
 
 
 def _generate_system_prompt(
@@ -85,60 +118,59 @@ async def get_chat_report(
     chat_messages: List[ChatMessage],
     rag_evaluator: RAGLanguageEvaluator,
 ) -> AsyncGenerator[str, None]:
-    context = ConversationContext(situation=situation, user=user)
-    system_prompt = _generate_system_prompt(context, rag_evaluator)
+    system_prompt = _generate_system_prompt_new(
+        ConversationContext(situation=situation, user=user)
+    )
 
-    relevant_examples: List[LanguageExample] = []
-    for msg in chat_messages:
-        if msg.role == "human":
-            examples = rag_evaluator.get_relevant_examples(
-                query=msg.content,
-                level=CEFR_LEVEL[user.language_level - 1],
-                k=3,
-            )
-            relevant_examples.extend(examples)
-
-    if relevant_examples:
-        system_prompt += "\n\nRelevant examples for this level and context:\n"
-        for example in relevant_examples:
-            system_prompt += f"• {example.phrase} (Context: {example.context})\n"
-
-    print(system_prompt)
+    examples = await _get_example_phrases(situation, rag_evaluator, user)
 
     prompt = ChatPromptTemplate.from_template(
         """{system_prompt}
-
-    Conversation:
-    {conversation}
 
     Analyze conversation and provide a report for the following topics:
 
     1. grammar: Grammatical accuracy
     2. vocabulary: Appropriate use of vocabulary for level
     3. fluency: Natural and smooth communication
+    4. goals: Achieving the user's goals
     
     Guidelines:
     - Compare the user's language with the provided example phrases for their level
     - Pay special attention to whether they use language appropriate for their level
-    - To evaluate give the result pay attention at the HUMAN messages
     - Look for appropriate use of formal/informal language based on the context
     - Remember to keep the same language as the conversation
+    - Only return the section of the report that corresponds to the metric
+
+    Conversation:
+    {conversation}
+
+    Goals of the user:
+    {goals}
+    
+    Examples of appropriate language for this level:
+    {examples}
     """,
     )
 
     # Format the conversation as a single string
     conversation_text = "\n".join(
-        [f"{msg.role.upper()}: {msg.content}" for msg in chat_messages]
+        [f'"{msg.content}"' for msg in chat_messages if msg.role == "human"]
     )
 
+    examples_text = "\n".join([f'"{msg.phrase}"' for msg in examples])
+
+    goals_text = "\n".join([f"• {goal}" for goal in situation.user_goals])
+
     # Create an LLM chain
-    chain = prompt | llm
+    chain = prompt | llm_feedback
 
     # Stream the response
     async for chunk in chain.astream(
         {
             "system_prompt": system_prompt,
             "conversation": conversation_text,
+            "examples": examples_text,
+            "goals": goals_text,
         }
     ):
         yield chunk
@@ -167,28 +199,29 @@ async def _get_conversation_metric(
         - NO additional text
         - The number represents the quality score for the metric
         - Higher numbers mean better quality
-        - Base the score on HUMAN messages only
         - Be critical in your evaluation
+               
 
         Conversation:
         {conversation}
+               
+        Examples of appropriate language for this level:
+        {examples}
 
         Evaluate and return a single number (0-100) for:
         {metric}
         """),
     )
 
-    llm.num_predict = 1
-
     # Create an LLM chain
     chain = prompt | llm
 
     # Format the conversation as a single string
     conversation_text = "\n".join(
-        [f"{msg.role.upper()}: {msg.content}" for msg in messages]
+        [f'"{msg.content}"' for msg in messages if msg.role == "human"]
     )
 
-    examples_text = "\n".join([msg.phrase for msg in examples])
+    examples_text = "\n".join([f'"{msg.phrase}"' for msg in examples])
 
     # Run the chain
     result = await chain.ainvoke(
@@ -200,12 +233,8 @@ async def _get_conversation_metric(
         },
     )
 
-    # Add error handling for non-numeric responses
-    try:
-        cleaned_result = result.strip().split()[0]  # Take first word only
-        return int(cleaned_result)
-    except (ValueError, IndexError):
-        return 50  # Default fallback value
+    cleaned_result = result.strip().split()[0]  # Take first word only
+    return int(cleaned_result)
 
 
 async def get_overview(
@@ -214,27 +243,7 @@ async def get_overview(
     messages: List[ChatMessage],
     rag_evaluator: RAGLanguageEvaluator,
 ) -> ConversationMetrics:
-    context = [situation.name, situation.scenario_description, *situation.user_goals]
-    examples = rag_evaluator.get_relevant_examples(
-        query=" ".join(context),
-        level=CEFR_LEVEL[user.language_level - 1],
-        k=5,
-    )
-
-    for example in examples:
-        translated_phrase = await translate_text(
-            example.phrase, user.current_language, user.language_level
-        )
-        example.phrase = translated_phrase
-
-    metrics = [
-        {"name": "grammar", "description": "Grammatical accuracy"},
-        {
-            "name": "vocabulary",
-            "description": "Appropriate use of vocabulary for level",
-        },
-        {"name": "fluency", "description": "Natural and smooth communication"},
-    ]
+    examples = await _get_example_phrases(situation, rag_evaluator, user)
 
     results = {}
 
